7087a9636fb9ecda090eb0b0c74199be2345e82e
Faisal Ahmed <faisal.dev@gmail.com>
2021-10-13 20:58:55 +0300

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          Please get deployed
         </a>
       </header>
     </div>
